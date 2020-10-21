c7bfc0eef282efb6003f772b7d8c0f49bb171a2a
Artur <62010277+NiezajetyNick@users.noreply.github.com>
2020-10-21 11:04:59 +0200

    Poprawa stylów, przyciemniania, dodano wyskakującą stronę

--- jotes.js
+++ jotes.js
@@ -10,28 +10,30 @@ let zmienna2 = 0;
 
 const karta3 = document.querySelector(".karta3");
 const tresc3 = document.getElementById("tresc3");
+const hadwa = document.getElementById("hadwa");
 const conc3 = document.querySelector(".conc3");
 let zmienna3 = 0;
+
+const prz = document.querySelector(".prz");
+
 
 karta.addEventListener("mouseover", function(){
     if(zmienna==0){
         tresc.className="pokaz";
+        karta.style.opacity="80%";
+        karta.style.zIndex="0";
+        prz.style.zIndex="1";
         tresc.style.animation = "wej 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
         zmienna++;
-    }else{
-        tresc.className="schowaj";
-        tresc.style.animation = "wyj 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
-        zmienna--;
     }
 });
 
 karta.addEventListener("mouseleave", function(){
-    if(zmienna==0){
+    if(zmienna==1){
         tresc.className="schowaj";
-        tresc.style.animation = "wej 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
-        zmienna++;
-    }else{
-        tresc.className="pokaz";
+        karta.style.opacity="100%";
+        karta.style.zIndex="0";
+        prz.style.zIndex="1";
         tresc.style.animation = "wyj 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
         zmienna--;
     }
@@ -42,23 +44,21 @@ karta.addEventListener("mouseleave", function(){
 karta2.addEventListener("mouseover", function(){
     if(zmienna2==0){
         tresc2.className="pokaz";
+        karta2.style.opacity="80%";
+        karta2.style.zIndex="0";
+        prz.style.zIndex="1";
         tresc2.style.animation = "wej 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
         zmienna2++;
-    }else{
-        tresc2.className="schowaj";
-        tresc2.style.animation = "wyj 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
-        zmienna2--;
     }
 });
 
 karta2.addEventListener("mouseleave", function(){
-    if(zmienna2==0){
+    if(zmienna2==1){
         tresc2.className="schowaj";
+        karta2.style.opacity="100%";
+        karta2.style.zIndex="0";
+        prz.style.zIndex="1";
         tresc2.style.animation = "wej 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
-        zmienna2++;
-    }else{
-        tresc2.className="pokaz";
-        tresc2.style.animation = "wyj 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
         zmienna2--;
     }
 });
@@ -68,23 +68,21 @@ karta2.addEventListener("mouseleave", function(){
 karta3.addEventListener("mouseover", function(){
     if(zmienna3==0){
         tresc3.className="pokaz";
+        karta3.style.opacity="80%";
+        karta3.style.zIndex="0";
+        prz.style.zIndex="1";
         tresc3.style.animation = "wej 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
         zmienna3++;
-    }else{
-        tresc3.className="schowaj";
-        tresc3.style.animation = "wyj 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
-        zmienna3--;
     }
 });
 
 karta3.addEventListener("mouseleave", function(){
-    if(zmienna3==0){
+    if(zmienna3==1){
         tresc3.className="schowaj";
+        karta3.style.opacity="100%";
+        karta3.style.zIndex="0";
+        prz.style.zIndex="1";
         tresc3.style.animation = "wej 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
-        zmienna3++;
-    }else{
-        tresc3.className="pokaz";
-        tresc3.style.animation = "wyj 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
         zmienna3--;
     }
 });
